2e87a8a495ae4e1a689e683c70cb3601771aab1a
leon <applepip@163.com>
2019-08-27 16:19:26 +0800

    rnn learning in several steps

--- tensorflow_rnn/bitcoin_data.py
+++
@@ -1,6 +0,0 @@
-from plot_coin_chart import *
-
-bitcoin_market_info = pd.read_csv('data/bitcoin.csv')
-
-print(bitcoin_market_info.head())
-plot_coin_chart(bitcoin_market_info, 'btc')
--- tensorflow_rnn/ethereum_data.py
+++
@@ -1,7 +0,0 @@
-from plot_coin_chart import *
-
-eth_market_info = pd.read_csv('data/ethereum.csv')
-
-print(eth_market_info.head())
-plot_coin_chart(eth_market_info, 'eth')
-
