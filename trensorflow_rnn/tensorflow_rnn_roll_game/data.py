9e591255ac9f9a780db9b565a2082667528ce356
leon <applepip@163.com>
2019-08-27 17:44:48 +0800

    modify the data bug

--- trensorflow_rnn/tensorflow_rnn_roll_game/data.py
+++ trensorflow_rnn/tensorflow_rnn_roll_game/data.py
@@ -2,13 +2,13 @@ import numpy as np
 from plot_data_distribution import *
 
 
-def gen_echo_data(datasize, batch_vertical_size, roll_steps=3):
+def gen_echo_data(datasize, batch_vertical_size, roll_steps = 3):
     '''
 	生成二进制数据流x，x沿x轴的正方向滚动roll_steps位形成y
 	'''
     x = np.array(np.random.choice(2, datasize, p=[0.5, 0.5])) #p=[0.5, 0.5] 取0,1的概率各为50%
     y = np.roll(x, roll_steps)
-	y[0:echo_step] = 0  #roll后，roll_steps前的数据设为0
+    y[0:roll_steps] = 0  #roll后，roll_steps前的数据设为0
 
     # 显示25个数据样本
     # plot_data_distribution(x[0:25], y[0:25])
--- trensorflow_rnn/tensorflow_rnn_roll_game_tf/data.py
+++ trensorflow_rnn/tensorflow_rnn_roll_game_tf/data.py
@@ -7,7 +7,7 @@ def gen_echo_data(datasize, batch_vertical_size, roll_steps = 3):
 	'''
     x = np.array(np.random.choice(2, datasize, p=[0.5, 0.5])) #p=[0.5, 0.5] 取0,1的概率各为50%
     y = np.roll(x, roll_steps)
-	y[0:echo_step] = 0  #roll后，roll_steps前的数据设为0
+    y[0:roll_steps] = 0  #roll后，roll_steps前的数据设为0
 
     # 显示25个数据样本
     # plot_data_distribution(x[0:25], y[0:25])
@@ -16,3 +16,4 @@ def gen_echo_data(datasize, batch_vertical_size, roll_steps = 3):
     y = y.reshape((batch_vertical_size, -1))
 
     return (x, y)
+
